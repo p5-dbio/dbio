db3d4c3af05a2edbe4c0ae1fd07428e772524213
Torsten Raudssus <torsten@raudss.us>
2026-05-05 05:19:41 +0200

    refactor: extract data type classification into DBIO::Storage::DBI::DataTypeClassifier
    The six native-data-type / LOB classification helpers (_native_data_type,
    _max_column_bytesize, _is_lob_type, _is_binary_lob_type, _is_text_lob_type,
    _is_binary_type) are pure regex/classification utilities with effectively
    zero coupling to DBI.pm internals. Moved them into a sibling C3 mixin so
    DBI.pm shrinks and the classification subsystem has a documented home.

--- lib/DBIO/Storage/DBI.pm
+++ lib/DBIO/Storage/DBI.pm
@@ -5,7 +5,7 @@ package DBIO::Storage::DBI;
 use strict;
 use warnings;
 
-use base qw/DBIO::Storage::QueryRewrite DBIO::Storage::DBI::Capabilities DBIO::Storage/;
+use base qw/DBIO::Storage::QueryRewrite DBIO::Storage::DBI::Capabilities DBIO::Storage::DBI::DataTypeClassifier DBIO::Storage/;
 use mro 'c3';
 
 use DBIO::Carp;
@@ -2972,37 +2972,6 @@ sub last_insert_id {
   $self->_dbh_last_insert_id ($self->_dbh, @_);
 }
 
-=head2 _native_data_type
-
-=over 4
-
-=item Arguments: $type_name
-
-=back
-
-This API is B<EXPERIMENTAL>, will almost definitely change in the future, and
-currently only used by L<::AutoCast|DBIO::Storage::DBI::AutoCast> and
-L<::Sybase::ASE|DBIO::Sybase::Storage::ASE>.
-
-The default implementation returns C<undef>, implement in your Storage driver if
-you need this functionality.
-
-Should map types from other databases to the native RDBMS type, for example
-C<VARCHAR2> to C<VARCHAR>.
-
-Types with modifiers should map to the underlying data type. For example,
-C<INTEGER AUTO_INCREMENT> should become C<INTEGER>.
-
-Composite types should map to the container type, for example
-C<ENUM(foo,bar,baz)> becomes C<ENUM>.
-
-=cut
-
-sub _native_data_type {
-  #my ($self, $data_type) = @_;
-  return undef
-}
-
 # Override in DB-specific subclasses for the correct RANDOM() function
 sub _random_function { 'RANDOM()' }
 
@@ -3511,82 +3480,6 @@ sub relname_to_table_alias {
     join('_', $relname, $join_count) : $relname);
 
   return $alias;
-}
-
-# The size in bytes to use for DBI's ->bind_param_inout, this is the generic
-# version and it may be necessary to amend or override it for a specific storage
-# if such binds are necessary.
-sub _max_column_bytesize {
-  my ($self, $attr) = @_;
-
-  my $max_size;
-
-  if ($attr->{sqlt_datatype}) {
-    my $data_type = lc($attr->{sqlt_datatype});
-
-    if ($attr->{sqlt_size}) {
-
-      # String/sized-binary types
-      if ($data_type =~ /^(?:
-          l? (?:var)? char(?:acter)? (?:\s*varying)?
-            |
-          (?:var)? binary (?:\s*varying)?
-            |
-          raw
-        )\b/x
-      ) {
-        $max_size = $attr->{sqlt_size};
-      }
-      # Other charset/unicode types, assume scale of 4
-      elsif ($data_type =~ /^(?:
-          national \s* character (?:\s*varying)?
-            |
-          nchar
-            |
-          univarchar
-            |
-          nvarchar
-        )\b/x
-      ) {
-        $max_size = $attr->{sqlt_size} * 4;
-      }
-    }
-
-    if (!$max_size and !$self->_is_lob_type($data_type)) {
-      $max_size = 100 # for all other (numeric?) datatypes
-    }
-  }
-
-  $max_size || $self->_dbio_connect_attributes->{LongReadLen} || $self->_get_dbh->{LongReadLen} || 8000;
-}
-
-# Determine if a data_type is some type of BLOB
-sub _is_lob_type {
-  my ($self, $data_type) = @_;
-  $data_type && ($data_type =~ /lob|bfile|text|image|bytea|memo/i
-    || $data_type =~ /^long(?:\s+(?:raw|bit\s*varying|varbit|binary
-                                  |varchar|character\s*varying|nvarchar
-                                  |national\s*character\s*varying))?\z/xi);
-}
-
-sub _is_binary_lob_type {
-  my ($self, $data_type) = @_;
-  $data_type && ($data_type =~ /blob|bfile|image|bytea/i
-    || $data_type =~ /^long(?:\s+(?:raw|bit\s*varying|varbit|binary))?\z/xi);
-}
-
-sub _is_text_lob_type {
-  my ($self, $data_type) = @_;
-  $data_type && ($data_type =~ /^(?:clob|memo)\z/i
-    || $data_type =~ /^long(?:\s+(?:varchar|character\s*varying|nvarchar
-                        |national\s*character\s*varying))\z/xi);
-}
-
-# Determine if a data_type is some type of a binary type
-sub _is_binary_type {
-  my ($self, $data_type) = @_;
-  $data_type && ($self->_is_binary_lob_type($data_type)
-    || $data_type =~ /(?:var)?(?:binary|bit|graphic)(?:\s*varying)?/i);
 }
 
 1;
